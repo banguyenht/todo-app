ba72407ee6a0dbed2d37bcf6a191125c66129211
ba nguyen <nguyenhuuba0797@gmail.com>
2021-03-09 21:37:49 +0700

    add auth

--- src/App.js
+++ src/App.js
@@ -1,115 +1,18 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import TaskControl from './components/TaskControl'
-import TaskList from './components/TaskList'
-import AddTask from './components/AddTask'
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      tasks: [],
-      isDisplay: false,
-      taskEditting: null
-    }
-  }
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-  componentDidMount() {
-    if(localStorage && localStorage.getItem('tasks')) {
-      const tasks = JSON.parse(localStorage.getItem('tasks'))
-      this.setState({ tasks: tasks })
-    }
-  }
-
-  randomId() {
-    return Math.random().toString(36).substr(2, 9)
-  }
-
-  onAddTask = () => {
-    this.setState({ isDisplay: true })
-  }
-
-  onHideClick = () => {
-    this.setState({ isDisplay: false, taskEditting: null })
-  }
-
-  onSubmit = (data) => {
-    var { tasks } = this.state
-    data.id = this.randomId()
-    tasks.push(data)
-    this.setState({
-      tasks: tasks,
-      isDisplay: false
-    })
-      localStorage.setItem('tasks', JSON.stringify(tasks))
-  }
-
-  onUpdateStatus = (taskId) => {
-    var index = this.findIndex(taskId)
-    var tasks = this.state.tasks
-    if(index != -1) {
-      tasks[index].status = !tasks[index].status
-      this.setState({tasks: tasks})
-      localStorage.setItem('tasks', JSON.stringify(tasks))
-    }
-  }
-
-
-  onDeleteItem = (taskId) => {
-    var index = this.findIndex(taskId)
-    var tasks = this.state.tasks
-    if(index != -1) {
-      tasks.splice(index, 1)
-      this.setState({task: tasks})
-      localStorage.setItem('tasks', JSON.stringify(tasks))
-    }
-  }
-
-  onUpdateItem = (taskId) => {
-    var index = this.findIndex(taskId)
-    var tasks= this.state.tasks
-    if(index != -1) {
-      this.setState({ isDisplay: true })
-      this.setState({taskEditting: tasks[index]})
-    }
-  }
-
-  findIndex = (id) => {
-    var tasks = this.state.tasks;
-    var result = -1
-    tasks.forEach((task, index) => {
-      if(task.id === id) {
-        result = index
-      }
-    })
-    return result
-  }
-
-  render() {
-    const { tasks, isDisplay } = this.state
-    const formTask = isDisplay ?
-      <AddTask onHideClick={this.onHideClick}
-        onSubmit={this.onSubmit}
-        taskEditting = {this.state.taskEditting}
-        /> : ''
-    const classForTable =  isDisplay ? 'col col-md-8' : 'col col-md-12'
-    return (
-      <div className="container">
-        <h4 className='text-center'>TODO APP</h4>
-        <div className='row'>
-          <div className='col col-md-4'>
-            { formTask }
-          </div>
-          <div className={classForTable}>
-            <button className='btn btn-primary' onClick={ this.onAddTask }>Add Task</button>
-            <TaskControl />
-            <TaskList tasks = {tasks} onUpdateStatus={this.onUpdateStatus} onDeleteItem = {this.onDeleteItem}
-              onUpdateItem = {this.onUpdateItem}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
+  );
 }
 
-export default App;
+export default withAuthenticator(App);
